e5953815a7c22aba55f675aff0144bca2a7db32b
FyiurAmron <spamove@gmail.com>
2025-01-04 19:22:49 +0100

    apply clang-format and additional formatting

--- SOURCE/DERBIS.C
+++ SOURCE/DERBIS.C
@@ -1,106 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-  
-#include "raptor.h"
+
 #include "file0001.inc"
+#include "raptor.h"
 
 #define MAX_DERBIS 15
 
-typedef struct DERB_S
-{
-   struct DERB_S *  prev;             // LINK LIST PREV
-   struct DERB_S *  next;             // LINK LIST NEXT
-   INT               dir;             // DIRECTION 0 - 7
-   INT               x;
-   INT               y;
-}DERBS;
+typedef struct DERB_S {
+    struct DERB_S* prev; // LINK LIST PREV
+    struct DERB_S* next; // LINK LIST NEXT
+    INT dir; // DIRECTION 0 - 7
+    INT x;
+    INT y;
+} DERBS;
 
-PRIVATE ESHOT     derbs [ MAX_DERBIS ];
-  
-PRIVATE ESHOT     first_derb;
-PRIVATE ESHOT     last_derb;
-PRIVATE ESHOT *   free_derb;
+PRIVATE ESHOT derbs[MAX_DERBIS];
+
+PRIVATE ESHOT first_derb;
+PRIVATE ESHOT last_derb;
+PRIVATE ESHOT* free_derb;
 
 /***************************************************************************
 DERB_Clear () - Clears out ESHOT Linklist
  ***************************************************************************/
-VOID
-DERB_Clear (
-VOID
-)
-{
-   INT loop;
-  
-   eshotnum = 0;
+VOID DERB_Clear( VOID ) {
+    INT loop;
 
-   first_derb.prev = NUL;
-   first_derb.next = &last_derb;
-  
-   last_derb.prev  = &first_derb;
-   last_derb.next  = NUL;
-  
-   free_derb       = eshots;
-  
-   memset ( eshots, 0, sizeof ( eshots ) );
-  
-   for ( loop = 0; loop < MAX_ESHOT-1; loop++ )
-      eshots[ loop ].next = &eshots [ loop + 1 ];
+    eshotnum = 0;
+
+    first_derb.prev = NUL;
+    first_derb.next = &last_derb;
+
+    last_derb.prev = &first_derb;
+    last_derb.next = NUL;
+
+    free_derb = eshots;
+
+    memset( eshots, 0, sizeof( eshots ) );
+
+    for ( loop = 0; loop < MAX_ESHOT - 1; loop++ ) {
+        eshots[loop].next = &eshots[loop + 1];
+    }
 }
 
 /*-------------------------------------------------------------------------*
 DERB_Get () - gets a Free ESHOT OBJECT from linklist
  *-------------------------------------------------------------------------*/
-PRIVATE ESHOT *
-DERB_Get (
-VOID
-)
-{
-   ESHOT * pNew;
-  
-   if ( !free_derb )
-      return ( NUL );
-  
-   eshotnum++;
-   if ( eshothigh < eshotnum )
-      eshothigh = eshotnum;
+PRIVATE ESHOT* DERB_Get( VOID ) {
+    ESHOT* pNew;
 
-   pNew = free_derb;
-   free_derb = free_derb->next;
-  
-   memset ( pNew, 0 ,sizeof ( ESHOT ) );
-  
-   pNew->next = &last_derb;
-   pNew->prev = last_derb.prev;
-   last_derb.prev = pNew;
-   pNew->prev->next = pNew;
-  
-   return ( pNew );
+    if ( !free_derb ) {
+        return NUL;
+    }
+
+    eshotnum++;
+    if ( eshothigh < eshotnum ) {
+        eshothigh = eshotnum;
+    }
+
+    pNew = free_derb;
+    free_derb = free_derb->next;
+
+    memset( pNew, 0, sizeof( ESHOT ) );
+
+    pNew->next = &last_derb;
+    pNew->prev = last_derb.prev;
+    last_derb.prev = pNew;
+    pNew->prev->next = pNew;
+
+    return pNew;
 }
-  
+
 /*-------------------------------------------------------------------------*
 DERB_Remove () - Removes SHOT OBJECT from linklist
  *-------------------------------------------------------------------------*/
-PRIVATE ESHOT *
-DERB_Remove (
-ESHOT * sh
-)
-{
-   ESHOT * next;
-  
-   eshotnum--;
+PRIVATE ESHOT* DERB_Remove( ESHOT* sh ) {
+    ESHOT* next;
 
-   next = sh->prev;
-  
-   sh->next->prev = sh->prev;
-   sh->prev->next = sh->next;
-  
-   memset ( sh, 0, sizeof ( ESHOT ) );
-  
-   sh->next = free_derb;
-  
-   free_derb = sh;
-  
-   return ( next );
+    eshotnum--;
+
+    next = sh->prev;
+
+    sh->next->prev = sh->prev;
+    sh->prev->next = sh->next;
+
+    memset( sh, 0, sizeof( ESHOT ) );
+
+    sh->next = free_derb;
+
+    free_derb = sh;
+
+    return next;
 }
-  
--- SOURCE/HELP.C
+++ SOURCE/HELP.C
@@ -1,159 +1,153 @@
+#include <conio.h>
+#include <dos.h>
+#include <io.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
-#include <io.h>
-#include <dos.h>
-  
-#include "raptor.h"
-#include "helpwin.inc"
+
 #include "file0000.inc"
 #include "file0001.inc"
+#include "helpwin.inc"
+#include "raptor.h"
 
 PRIVATE DWORD startitem;
-PRIVATE INT   curpage;
-PRIVATE INT   maxpages;
+PRIVATE INT curpage;
+PRIVATE INT maxpages;
 
 /***************************************************************************
 HELP_Init() - inits the help stuff
  ***************************************************************************/
-VOID
-HELP_Init (
-VOID
-)
-{
-   INT enditem;
+VOID HELP_Init( VOID ) {
+    INT enditem;
 
-   startitem = GLB_GetItemID ("STARTHELP");
-   enditem = GLB_GetItemID ("ENDHELP");
+    startitem = GLB_GetItemID( "STARTHELP" );
+    enditem = GLB_GetItemID( "ENDHELP" );
 
-   if ( !reg_flag )
-      startitem += (DWORD)2;
+    if ( !reg_flag ) {
+        startitem += (DWORD) 2;
+    }
 
-   maxpages = ( enditem - startitem - 1 );
+    maxpages = enditem - startitem - 1;
 
-   startitem++;
+    startitem++;
 
-   curpage = 0;
+    curpage = 0;
 }
 
 /***************************************************************************
 HELP_Win() - Displays the help window at the specified page
  ***************************************************************************/
-VOID
-HELP_Win(
-CHAR * strpage             // INPUT : GLB string item
-)
-{
-   CHAR           temp[20];
-   SWD_DLG        dlg;
-   INT            window;
-   BOOL           update = TRUE;
-   DWORD          item;
+VOID HELP_Win(
+    CHAR* strpage // INPUT : GLB string item
+) {
+    CHAR temp[20];
+    SWD_DLG dlg;
+    INT window;
+    BOOL update = TRUE;
+    DWORD item;
 
-   item = GLB_GetItemID ( strpage );
+    item = GLB_GetItemID( strpage );
 
-   if ( item == EMPTY )
-      EXIT_Error ("HELP() - Invalid Page");
-   
-   curpage = item - startitem;
+    if ( item == EMPTY ) {
+        EXIT_Error( "HELP() - Invalid Page" );
+    }
 
-   KBD_Clear();
-   window = SWD_InitWindow ( HELP_SWD );
+    curpage = item - startitem;
 
-   SND_Patch ( FX_DOOR, 127 );
+    KBD_Clear();
+    window = SWD_InitWindow( HELP_SWD );
 
-   mainloop:
-  
-   if ( update )
-   {
-      update = FALSE;
+    SND_Patch( FX_DOOR, 127 );
 
-      if ( curpage >= 0 )
-         curpage = curpage % maxpages;
-      else
-         curpage = maxpages + curpage;
+mainloop:
 
-      SWD_SetFieldItem ( window, HELP_TEXT, startitem + (DWORD)curpage );
+    if ( update ) {
+        update = FALSE;
 
-      sprintf (temp, "PAGE : %02u", curpage+1 );
-      SWD_SetFieldText ( window, HELP_HEADER, temp );
+        if ( curpage >= 0 ) {
+            curpage = curpage % maxpages;
+        } else {
+            curpage = maxpages + curpage;
+        }
 
-      SWD_ShowAllWindows();
-      GFX_DisplayUpdate();
-   }
+        SWD_SetFieldItem( window, HELP_TEXT, startitem + (DWORD) curpage );
 
-   SWD_Dialog ( &dlg );
+        sprintf( temp, "PAGE : %02u", curpage + 1 );
+        SWD_SetFieldText( window, HELP_HEADER, temp );
 
-   switch ( dlg.keypress )
-   {
-      case SC_HOME:
-         update = TRUE;
-         curpage = 0;
-         break;
+        SWD_ShowAllWindows();
+        GFX_DisplayUpdate();
+    }
 
-      case SC_F1:
-         update = TRUE;
-         curpage = 1;
-         break;
+    SWD_Dialog( &dlg );
 
-      case SC_END:
-         update = TRUE;
-         curpage = maxpages-1;
-         break;
+    switch ( dlg.keypress ) {
+        case SC_HOME:
+            update = TRUE;
+            curpage = 0;
+            break;
 
-      case SC_DOWN:
-      case SC_RIGHT:
-      case SC_PAGEDN:
-         update = TRUE;
-         curpage++;
-         break;
+        case SC_F1:
+            update = TRUE;
+            curpage = 1;
+            break;
 
-      case SC_UP:
-      case SC_LEFT:
-      case SC_PAGEUP:
-         update = TRUE;
-         curpage--;
-         break;
-   }
+        case SC_END:
+            update = TRUE;
+            curpage = maxpages - 1;
+            break;
 
-   if ( KBD_IsKey ( SC_ESC ) )
-      goto func_exit;
+        case SC_DOWN:
+        case SC_RIGHT:
+        case SC_PAGEDN:
+            update = TRUE;
+            curpage++;
+            break;
 
-   if ( KBD_Key ( SC_X ) && KBD_Key ( SC_ALT ) )
-      WIN_AskExit();
-  
-   switch ( dlg.cur_act )
-   {
-      case S_FLD_COMMAND:
-      switch ( dlg.cur_cmd )
-      {
-         case F_SELECT:
-         switch ( dlg.field )
-         {
-            case HELP_DONE:
-               goto func_exit;
+        case SC_UP:
+        case SC_LEFT:
+        case SC_PAGEUP:
+            update = TRUE;
+            curpage--;
+            break;
+    }
 
-            case HELP_DOWN:
-               update = TRUE;
-               curpage++;
-               break;
+    if ( KBD_IsKey( SC_ESC ) ) {
+        goto func_exit;
+    }
 
-            case HELP_UP:
-               update = TRUE;
-               curpage--;
-               break;
-         }
-      }
-   }
+    if ( KBD_Key( SC_X ) && KBD_Key( SC_ALT ) ) {
+        WIN_AskExit();
+    }
 
-   goto mainloop;
+    switch ( dlg.cur_act ) {
+        case S_FLD_COMMAND:
+            switch ( dlg.cur_cmd ) {
+                case F_SELECT:
+                    switch ( dlg.field ) {
+                        case HELP_DONE:
+                            goto func_exit;
 
-   func_exit:
-  
-   SWD_DestroyWindow ( window );
-   SWD_ShowAllWindows();
-   GFX_DisplayUpdate();
-  
-   return;
+                        case HELP_DOWN:
+                            update = TRUE;
+                            curpage++;
+                            break;
+
+                        case HELP_UP:
+                            update = TRUE;
+                            curpage--;
+                            break;
+                    }
+            }
+    }
+
+    goto mainloop;
+
+func_exit:
+
+    SWD_DestroyWindow( window );
+    SWD_ShowAllWindows();
+    GFX_DisplayUpdate();
+
+    return;
 }
