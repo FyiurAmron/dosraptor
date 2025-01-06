a287594b38a96994954670761b71067e15893027
FyiurAmron <spamove@gmail.com>
2025-01-06 13:40:51 +0100

    src: switch tile_a from asm to C

---
+++ SOURCE/tile_a.c
@@ -0,0 +1,36 @@
+
+#include "tile_a.h"
+
+#include <string.h>
+
+extern uint8_t* displaybuffer;
+extern uint8_t* displayscreen;
+
+extern uint8_t* tilepic;
+extern uint8_t* tilestart;
+
+extern int32_t g_mapleft;
+
+void TILE_Draw( int max_y ) {
+    uint8_t* src = tilepic;
+    uint8_t* dst = tilestart;
+
+    int i;
+    for ( i = 0; i < max_y; i++ ) {
+        memcpy( dst, src, TILEWIDTH );
+        src += TILEWIDTH;
+        dst += SCREENWIDTH;
+    }
+}
+
+void TILE_DisplayScreen( int srcOffset, int dstOffset, int cols  ) {
+    uint8_t* src = displaybuffer + srcOffset;
+    uint8_t* dst = displayscreen + dstOffset;
+
+    int i;
+    for ( i = 0; i < SCREENHEIGHT; i++ ) {
+        memcpy( dst, src, cols );
+        dst += SCREENWIDTH;
+        src += SCREENWIDTH;
+    }
+}
---
+++ SOURCE/tile_a.h
@@ -0,0 +1,19 @@
+//
+// Created by m on 2025-01-05.
+//
+
+#ifndef TILE_A_H
+#define TILE_A_H
+
+#define SCREENWIDTH  320
+#define SCREENHEIGHT 200
+#define TILEWIDTH    32
+#define TILEHEIGHT   32
+#define MAP_LEFT     16
+
+#include "..\gfx\types.h"
+
+void TILE_Draw( int max_y );
+void TILE_DisplayScreen( int srcOffset, int dstOffset, int cols  );
+
+#endif // TILE_A_H
