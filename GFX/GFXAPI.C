40f24d5b87982b04598a14f2bdd3b51a6f97cf25
FyiurAmron <spamove@gmail.com>
2025-01-11 20:17:29 +0100

    merge _a code (migrated from asm) into base C code

--- GFX/GFXAPI.C
+++ GFX/GFXAPI.C
@@ -29,6 +29,9 @@
 
 #define G3D_DIST 200
 
+#define SCREENWIDTH  320
+#define SCREENHEIGHT 200 // unused here
+
 PUBLIC volatile INT framecount = 0;
 PUBLIC INT g_rseed = 1;
 PUBLIC DWORD ylookup[SCREENHEIGHT];
@@ -1055,24 +1058,6 @@ void GFX_Rectangle(
     GFX_VLine( x + lx - 1, y + 1, ly - 2, color );
 }
 
-void GFX_ScalePic(
-    BYTE* src, // INPUT : pointer to pic data
-    INT x, // INPUT : x display position
-    INT y, // INPUT : y display position
-    INT new_lx, // INPUT : new x length
-    INT new_ly, // INPUT : new y length
-    BOOL see_thru // INPUT : TRUE = see thru
-) {
-    GFX_PIC* h = (GFX_PIC*) src;
-
-    if ( h->type == GSPRITE ) {
-        GFX_PutSprite( src, x, y );
-        return;
-    }
-
-    EXIT_Error( "trying to use obsolete GFX_ScalePic functionality" );
-}
-
 /*************************************************************************
    GFX_MarkUpdate () Marks an area to be draw with GFX_DrawScreen()
  *************************************************************************/
@@ -1486,4 +1471,115 @@ void GFX_3DPoint( void ) {
 
     G3D_screenx += G3D_viewx;
     G3D_screeny += G3D_viewy;
+}
+
+// gfxapi_a
+
+void GFX_Shade( uint8_t* dst, int maxlen, const uint8_t* shading_table ) {
+    for ( int i = 0; i < maxlen; i++ ) {
+        *dst = shading_table[*dst];
+        dst++;
+    }
+}
+
+void GFX_ShadeSprite( uint8_t* dst, const uint8_t* src, const uint8_t* shading_table ) {
+    int offset;
+    while ( ( offset = ( (GFX_SPRITE*) src )->offset ) != -1 ) {
+        const int length = ( (GFX_SPRITE*) src )->length;
+        src += sizeof( GFX_SPRITE );
+
+        for ( int i = 0; i < length; i++ ) {
+            dst[offset] = shading_table[dst[offset]];
+            offset++;
+        }
+        src += length;
+    }
+}
+
+void GFX_DrawSprite( uint8_t* dst, const uint8_t* src ) {
+    int offset;
+    while ( ( offset = ( (GFX_SPRITE*) src )->offset ) != -1 ) {
+        const int length = ( (GFX_SPRITE*) src )->length;
+        src += sizeof( GFX_SPRITE );
+        memcpy( dst + offset, src, length );
+        src += length;
+    }
+}
+
+void GFX_DrawChar( uint8_t* dst, const uint8_t* src, int lx, int ly, int add_x_src, uint8_t color ) {
+    int add_x_dst = SCREENWIDTH - lx;
+
+    for ( int y = 0; y < ly; y++ ) {
+        for ( int x = 0; x < lx; x++ ) {
+            uint8_t pixel = *src++;
+            if ( pixel != 0 ) {
+                *dst = pixel + color;
+            }
+            dst++;
+        }
+        src += add_x_src;
+        dst += add_x_dst;
+    }
+}
+
+inline uint8_t* GFX_GetDst() {
+    return displaybuffer + gfx_xp + gfx_yp * SCREENWIDTH;
+}
+
+void GFX_PutPic( void ) {
+    uint8_t* src = gfx_inmem;
+    uint8_t* dst = GFX_GetDst();
+
+    if ( gfx_lx < 1 ) {
+        return;
+    }
+
+    for ( int y = 0; y < gfx_ly; y++ ) {
+        memcpy( dst, src, gfx_lx );
+        dst += SCREENWIDTH;
+        src += gfx_lx + gfx_imga; // ca. h->width
+    }
+}
+
+void GFX_PutMaskPic( void ) {
+    uint8_t* src = gfx_inmem;
+    uint8_t* dst = GFX_GetDst();
+
+    if ( gfx_lx < 1 ) {
+        return;
+    }
+
+    for ( int y = 0; y < gfx_ly; y++ ) {
+        for ( int x = 0; x < gfx_lx; x++ ) {
+            int al = src[x];
+            if ( al != 0 ) {
+                dst[x] = al;
+            }
+        }
+
+        dst += SCREENWIDTH;
+        src += gfx_imga; // no gfx_lx added here?!
+    }
+}
+
+void GFX_DisplayScreen( void ) {
+    int offset = ud_x + ud_y * SCREENWIDTH;
+    uint8_t* src = displaybuffer + offset;
+    uint8_t* dst = displayscreen + offset;
+
+    if ( ud_lx < 1 ) {
+        return;
+    }
+
+    if ( ud_lx == SCREENWIDTH ) {
+        memcpy( dst, src, SCREENWIDTH * ud_ly );
+    } else {
+        for ( int y = 0; y < ud_ly; y++ ) {
+            memcpy( dst, src, ud_lx );
+            dst += SCREENWIDTH;
+            src += SCREENWIDTH;
+        }
+    }
+
+    update_start = FALSE;
 }
--- GFX/PTRAPI.C
+++ GFX/PTRAPI.C
@@ -708,3 +708,92 @@ void PTR_End( void ) {
     regs.x.eax = 0x0;
     int386( 0x33, &regs, &regs );
 }
+
+// ptrapi_a
+
+void PTR_ReadJoyStick( void ) {
+    __asm {
+        pushad
+        mov edx, 0x201
+        xor edi, edi
+        xor esi, esi
+        xor ebx, ebx
+        xor eax, eax
+        mov ecx, 0x3FF
+
+        out dx, al
+        in al, dx
+        in al, dx
+        not al
+        shr eax, 4
+        mov joy_buttons, eax
+        xor eax, eax
+
+    LR_1:
+        in al, dx
+        and al, 0xF
+        jz LR_2
+
+        rcr al, 1
+        adc edi, ebx
+        rcr al, 1
+        adc esi, ebx
+        loop LR_1
+
+    LR_2:
+        mov joy_y, esi
+        mov joy_x, edi
+
+        popad
+    }
+}
+
+void PTR_Copy( const uint8_t* src, uint8_t* dst, int max_y ) {
+    for ( int y = 0; y < max_y; y++ ) {
+        memcpy( dst, src, CURSORWIDTH );
+        dst += CURSORWIDTH;
+        src += SCREENWIDTH;
+    }
+}
+
+void PTR_Save( int max_y ) {
+    PTR_Copy( cursorstart, cursorsave, max_y );
+}
+
+void PTR_Erase( void ) {
+    uint8_t* src = cursorsave;
+    uint8_t* dst = cursorstart;
+    for ( int y = 0; y < CURSORHEIGHT; y++ ) {
+        memcpy( dst, src, CURSORWIDTH );
+        dst += SCREENWIDTH;
+        src += CURSORWIDTH;
+    }
+}
+
+void PTR_ClipErase( void ) {
+    uint8_t* src = cursorsave;
+    uint8_t* dst = cursorstart;
+
+    for ( int y = 0; y < cursorloopy; y++ ) {
+        memcpy( dst, src, cursorloopx );
+        dst += SCREENWIDTH;
+        src += CURSORWIDTH;
+    }
+}
+
+void PTR_Draw( void ) {
+    uint8_t* src = displaypic;
+    uint8_t* dst = cursorstart;
+
+    for ( int y = 0; y < cursorloopy; y++ ) {
+        for ( int x = 0; x < cursorloopx; x++ ) {
+            uint8_t pixel = *src++;
+            if ( pixel != 0 ) {
+                *dst = pixel;
+            }
+            dst++;
+        }
+        src += CURSORWIDTH - cursorloopx;
+        dst += SCREENWIDTH - cursorloopx;
+    }
+}
